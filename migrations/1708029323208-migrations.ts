ed856b8369ca7484eecde663aa0d3e418a9cbc05
Ihor Sokolov <i.sokolov@geniusee.com>
2024-02-19 22:38:12 +0200

    update migrations

---
+++ migrations/1708029323208-migrations.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Migrations1708029323208 implements MigrationInterface {
+  name = 'Migrations1708029323208';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "email" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
+}
--- migrations/index.ts
+++ migrations/index.ts
@@ -1,4 +1,11 @@
 import { Migrations1707249095812 } from './1707249095812-migrations';
 import { Migrations1707571510707 } from './1707571510707-migrations';
+import { Migrations1708029323208 } from './1708029323208-migrations';
+import { Migrations1708032790019 } from './1708032790019-migrations';
 
-export default [Migrations1707249095812, Migrations1707571510707];
+export default [
+  Migrations1707249095812,
+  Migrations1707571510707,
+  Migrations1708029323208,
+  Migrations1708032790019,
+];
