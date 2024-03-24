5f3426fbd62769e028defe6b0f63073702b99131
Ihor Sokolov <i.sokolov@geniusee.com>
2024-03-24 14:34:54 +0200

    add migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -6,6 +6,7 @@ import { Migrations1708721596106 } from './1708721596106-migrations';
 import { Migrations1709066677682 } from './1709066677682-migrations';
 import { Migrations1709748090507 } from './1709748090507-migrations';
 import { Migrations1709907013160 } from './1709907013160-migrations';
+import { Migrations1710797360569 } from './1710797360569-migrations';
 
 export default [
   Migrations1707249095812,
@@ -16,4 +17,5 @@ export default [
   Migrations1709066677682,
   Migrations1709748090507,
   Migrations1709907013160,
+  Migrations1710797360569,
 ];
