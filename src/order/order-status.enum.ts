ce03ff661c0dd05ba867f59796669996b04bb4c0
Ihor Sokolov <i.sokolov@geniusee.com>
2024-06-04 21:11:00 +0300

    migrate model name

--- src/order/order-status.enum.ts
+++
@@ -1,9 +0,0 @@
-import { registerEnumType } from '@nestjs/graphql';
-
-export enum OrderStatus {
-  NEW = 'NEW',
-  APPROVED = 'APPROVED',
-  DECLINED = 'DECLINED',
-}
-
-registerEnumType(OrderStatus, { name: 'OrderStatus' });
