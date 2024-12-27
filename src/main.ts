b04713e632094eb5c09cfe6c6fd01649ce932812
Ihor Sokolov <i.sokolov@geniusee.com>
2024-12-27 16:10:40 +0200

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
