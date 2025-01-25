cfdbe135a841ee100852d4ea73bf292997e84458
Ihor Sokolov <i.sokolov@geniusee.com>
2025-01-26 00:07:47 +0200

    finish review logic

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -5,9 +5,10 @@ import { ReviewResolver } from './api/review.query';
 import { ReviewMutations } from './api/review.mutation';
 import { Review } from './review.entity';
 import { RestaurantModule } from 'restaurant/restaurant.module';
+import { ReviewOwnerGuard } from './review-owner.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Review]), RestaurantModule],
-  providers: [ReviewService, ReviewResolver, ReviewMutations],
+  providers: [ReviewService, ReviewResolver, ReviewMutations, ReviewOwnerGuard],
 })
 export class ReviewModule {}
